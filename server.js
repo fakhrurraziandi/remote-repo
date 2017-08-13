3aa16373376ab6b1ec28053cbf34d81658fbb88a
fakhrurraziandi <fakhrurrazi.andi@gmail.com>
2017-08-13 15:07:05 +0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+
+var http = require('http');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello world!');
+    res.end();
+}).listen(3000, function(){
+    console.log('Server is listening on port 3000');
+});
